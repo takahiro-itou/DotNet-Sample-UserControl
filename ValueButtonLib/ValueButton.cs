c304ef34fdf7152d0ffe1a0dda0435851ee15581
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-09-16 21:41:19 +0900

    プロパティを追加

--- ValueButtonLib/ValueButton.cs
+++ ValueButtonLib/ValueButton.cs
@@ -2,6 +2,13 @@
 {
     public partial class ValueButton : Button
     {
+        private int varValue;
+        public int ButtonValue
+        {
+            get { return varValue; }
+            set { varValue = value; }
+        }
+
         public ValueButton()
         {
             InitializeComponent();
