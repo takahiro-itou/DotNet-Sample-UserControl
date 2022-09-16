859ce3c2340e6c8da97aca5c6d0f9d56eea2d26f
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-09-16 21:38:06 +0900

    存在しないプロパティを割り当てるコードを削除

--- ValueButtonLib/ValueButton.Designer.cs
+++ ValueButtonLib/ValueButton.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
         }
 
